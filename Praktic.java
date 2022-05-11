7165771268c9d3d623a0512e8af49cdf0a2dd6c8
Michail <r4spberrycre4m@gmail.com>
2022-05-11 18:53:25 +0500

    Обновление файлов на git

--- Praktic.java
+++ Praktic.java
@@ -1,2 +1,18 @@
 public class Practic {
     public static void main(String[] args) {
+public static void main(String[] args) {
+		ArrayList<Integer> temp = new ArrayList<>();
+		temp.add(120);
+		temp.add(75);
+		temp.add(42);
+		temp.add(60);
+		temp.add(110);
+		temp.add(20);
+		
+		int sum = 0;
+		for (int i = 0; i < temp.size(); i++){
+		    sum = sum + temp[i];
+		}
+		int averageSpeed = sum / temp.size();
+		System.out.println("Средняя скорость " + averageSpeed);
+	}
